2376c69a1afd9a55b86e6f5118239d711772c446
Shrouk Ashraf <shroukashraf20002018@gmail.com>
2024-04-21 22:02:39 +0200

    Revert "create  & add rockets screen"

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
-
-
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'features/rockets/presentation/screens/rocket_details_screen.dart';
-
-
-import 'package:space_app/features/onboarding/views/onboarding_screen.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:space_app/features/onboarding/screens/onboarding_screen.dart';
 
-
 void main() {
   runApp(const SpaceApp());
 }
-
 
 class SpaceApp extends StatelessWidget {
   const SpaceApp({super.key});
@@ -28,7 +17,6 @@ class SpaceApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: "Orienta"),
         home: OnboardingScreen(),
-
       ),
     );
   }
