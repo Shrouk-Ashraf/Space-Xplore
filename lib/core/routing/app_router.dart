26a1b20851b84fc81f4bd20f575eb69e9451f5f0
Shrouk <shrouk.a.elsaeid31@gmail.com>
2024-04-28 03:40:16 +0300

    Add app router

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:space_app/core/di/dependency_injection.dart';
+import 'package:space_app/core/routing/routes.dart';
+import 'package:space_app/features/company%20info/ui/company_info_screen.dart';
+import 'package:space_app/features/crew/ui/screens/crew_screen.dart';
+import 'package:space_app/features/launches/logic/cubit/launch_cubit.dart';
+import 'package:space_app/features/launches/ui/screens/launch_details_screen.dart';
+import 'package:space_app/features/launches/ui/screens/launches_screen.dart';
+import 'package:space_app/features/onboarding/ui/screens/onboarding_screen.dart';
+import 'package:space_app/features/rockets/ui/screens/rockets_screen.dart';
+import 'package:space_app/features/ships/ui/screens/ships_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    //This arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case Routes.rocketsScreen:
+        return MaterialPageRoute(
+          builder: (_) => const RocketsScreen(),
+        );
+      case Routes.launchesScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LaunchCubit>(),
+            child: const LaunchesScreen(),
+          ),
+        );
+      case Routes.launchDetailsScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LaunchDetailsScreen(),
+        );
+      case Routes.crewScreen:
+        return MaterialPageRoute(
+          builder: (_) => const CrewScreen(),
+        );
+      case Routes.shipsScreen:
+        return MaterialPageRoute(
+          builder: (_) => ShipsScreen(),
+        );
+      case Routes.companyInfoScreen:
+        return MaterialPageRoute(
+          builder: (_) => const CompanyInfoScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String onBoardingScreen = '/onBoardingScreen';
+  static const String rocketsScreen = '/rocketsScreen';
+  static const String launchesScreen = '/launchesScreen';
+  static const String launchDetailsScreen = '/launchesDetailsScreen';
+  static const String crewScreen = '/crewScreen';
+  static const String shipsScreen = '/shipsScreen';
+  static const String companyInfoScreen = '/companyInfoScreen';
+}
