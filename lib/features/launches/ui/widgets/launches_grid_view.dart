0e4d1a3256b8a17f22bd2a235cd72c2f8a1f6895
Shrouk Ashraf <shrouk.a.elsaeid31@gmail.com>
2024-05-04 05:20:04 +0300

    add animation when navigate to details screen

--- lib/features/launches/ui/widgets/launches_grid_view.dart
+++ lib/features/launches/ui/widgets/launches_grid_view.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:space_app/core/routing/app_router.dart';
+import 'package:space_app/core/routing/routes.dart';
 import 'package:space_app/features/launches/data/models/launch_response.dart';
 import 'package:space_app/features/launches/logic/cubit/launch_cubit.dart';
 import 'package:space_app/features/launches/ui/screens/launch_details_screen.dart';
@@ -56,13 +59,12 @@ class _LaunchesGridViewState extends State<LaunchesGridView> {
                 child: FadeInAnimation(
                   child: GestureDetector(
                     onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return LaunchDetailsScreen(
-                              item: widget.allLaunches[index],
-                            );
-                          },
+                      PersistentNavBarNavigator.pushNewScreen(
+                        context,
+                        withNavBar: false,
+                        pageTransitionAnimation: PageTransitionAnimation.sizeUp,
+                        screen: LaunchDetailsScreen(
+                          item: widget.allLaunches[index],
                         ),
                       );
                     },
